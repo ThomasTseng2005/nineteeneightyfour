0adb919407d576d20312e26edfc898b7f6b82d72
Thomas2030 <0830thomastseng@gmail.com>
2020-04-15 20:56:35 +0800

    third commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request, session, send_from_directory
 
 app = Flask(__name__)
 
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("unfound.html")
 
 if __name__ == '__main__':
     app.run(debug = True)
